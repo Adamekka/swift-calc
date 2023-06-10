651ce0cd755420729c423ea68bec9ffd859a1ad2
Adam Cvikl <adam.wasd4@gmail.com>
2023-06-10 13:56:04 +0200

    add `SwiftTUI` dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "swift-calc",
+    dependencies: [
+        .package(url: "https://github.com/rensbreur/SwiftTUI.git", from: "0.1.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
